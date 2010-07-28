7639383a8e361dbe65ac118c65c61777190ee814
Pascal Rettig <pascal@cykod.com>
2010-07-28 13:25:28 -0400

    Two substeps to write config

--- app/plans/steps/launch/write_server_config_files.rb
+++ app/plans/steps/launch/write_server_config_files.rb
@@ -2,7 +2,7 @@
 
 class Steps::Launch::WriteServerConfigFiles < Steps::Base
 
-  step_info "(S7) Write server configuration files"
+  step_info "(S7) Write server configuration files", :substeps => 2
 
   class Options < HashModel
 
@@ -10,88 +10,101 @@ class Steps::Launch::WriteServerConfigFiles < Steps::Base
 
   def execute!(step)
     
-    # need to write cms.yml, cms_migrator.yml, defaults.yml, workling.yml
     servers = deployment.servers
 
     fail_step("No servers") unless servers.length > 0
 
-    queue_servers = cloud.queue_servers
-    memcache_servers = cloud.memcache_servers
-    master_db = cloud.master_db
+    if step.substep == 0
+      # need to write cms.yml, cms_migrator.yml, defaults.yml, workling.yml
 
-    workling_yaml_data = {
-      'production' => { 'listens_on' => queue_servers.map { |s| "#{s.private_hostname}:15151" }.join(", ") },
-      'development' => { 'listens_on' => queue_servers.map { |s| "#{s.private_hostname}:22122" }.join(", ") }
-    }
+      queue_servers = cloud.queue_servers
+      memcache_servers = cloud.memcache_servers
+      master_db = cloud.master_db
 
-    defaults_yaml_data = {
-      'default_language' => 'en',
-      'default_country' => 'US',
-      'active_cache' => true,
-      'use_x_send_file' => true,
-      'enable_beta_code' => true,
-      'system_admin' => 'pascal@cykod.com',
-      'memcache_servers' => memcache_servers.map { |s| "#{s.private_hostname}:11211" }
-    }
+      workling_yaml_data = {
+        'production' => { 'listens_on' => queue_servers.map { |s| "#{s.private_hostname}:15151" }.join(", ") },
+        'development' => { 'listens_on' => queue_servers.map { |s| "#{s.private_hostname}:22122" }.join(", ") }
+      }
 
-    db_config = {
-      'encoding' => 'utf8',
-      'adapter' => 'mysql',
-      'database' => 'webiva',
-      'host' => master_db.hostname,
-      'socket' => '/var/run/mysqld/mysqld.sock',
-      'pool' => 8
-    }
+      defaults_yaml_data = {
+        'default_language' => 'en',
+        'default_country' => 'US',
+        'active_cache' => true,
+        'use_x_send_file' => true,
+        'enable_beta_code' => true,
+        'system_admin' => 'pascal@cykod.com',
+        'memcache_servers' => memcache_servers.map { |s| "#{s.private_hostname}:11211" }
+      }
 
-    cms_yaml_data = {
-      'production' => db_config.merge('username' => cloud.config.db_user,
-                                      'password' => cloud.config.db_user_password),
+      db_config = {
+        'encoding' => 'utf8',
+        'adapter' => 'mysql',
+        'database' => 'webiva',
+        'host' => master_db.hostname,
+        'socket' => '/var/run/mysqld/mysqld.sock',
+        'pool' => 12
+      }
 
-      'development' => db_config.merge('username' => cloud.config.db_user,
-                                      'password' => cloud.config.db_user_password)
-    }
+      cms_yaml_data = {
+        'production' => db_config.merge('username' => cloud.config.db_user,
+                                        'password' => cloud.config.db_user_password),
 
-    cms_migrator_yaml_data = {
-      'production' => db_config.merge('username' => cloud.config.migrator_user,
-                                      'password' => cloud.config.migrator_user_password),
+                                        'development' => db_config.merge('username' => cloud.config.db_user,
+                                                                         'password' => cloud.config.db_user_password)
+      }
 
-      'development' => db_config.merge('username' => cloud.config.migrator_user,
-                                      'password' => cloud.config.migrator_user_password)
- 
-    }
+      cms_migrator_yaml_data = {
+        'production' => db_config.merge('username' => cloud.config.migrator_user,
+                                        'password' => cloud.config.migrator_user_password),
 
-    servers.each do |server|
-       ssh = server.ssh
-       sftp = ssh.sftp 
+                                        'development' => db_config.merge('username' => cloud.config.migrator_user,
+                                                                         'password' => cloud.config.migrator_user_password)
 
-       ssh.exec!('sudo chmod -R a+w /home/webiva/shared/config/')
+      }
 
-       sftp.file.open("/home/webiva/shared/config/workling.yml","w") do |f|
-         f.puts(YAML.dump(workling_yaml_data))
-       end
+      servers.each do |server|
+        ssh = server.ssh
+        sftp = ssh.sftp 
 
-       sftp.file.open("/home/webiva/shared/config/defaults.yml","w") do |f|
-         f.puts(YAML.dump(defaults_yaml_data.merge(
-               'starling' => "#{server.private_hostname}:15151"
-         )))
-       end
+        ssh.exec!('sudo chmod -R a+w /home/webiva/shared/config/')
 
-       sftp.file.open("/home/webiva/shared/config/cms.yml","w") do |f|
-         f.puts(YAML.dump(cms_yaml_data))
-       end
+        sftp.file.open("/home/webiva/shared/config/workling.yml","w") do |f|
+          f.puts(YAML.dump(workling_yaml_data))
+        end
 
-       sftp.file.open('/home/webiva/shared/config/cms_migrator.yml','w') do |f|
-         f.puts(YAML.dump(cms_migrator_yaml_data))
-       end
+        sftp.file.open("/home/webiva/shared/config/defaults.yml","w") do |f|
+          f.puts(YAML.dump(defaults_yaml_data.merge(
+            'starling' => "#{server.private_hostname}:15151"
+          )))
+        end
 
-       sftp.file.open('/home/webiva/shared/config/server.yml','w') do |f|
-         f.puts(YAML.dump({ 'server' => { 'name' => server.private_hostname,
-                                          'type' => 'slave',
-                                          'roles' => server.roles } }))
-         
-       end
+        sftp.file.open("/home/webiva/shared/config/cms.yml","w") do |f|
+          f.puts(YAML.dump(cms_yaml_data))
+        end
 
-       ssh.exec!("cd /home/webiva/current; ./script/update_server_info.rb")
+        sftp.file.open('/home/webiva/shared/config/cms_migrator.yml','w') do |f|
+          f.puts(YAML.dump(cms_migrator_yaml_data))
+        end
+
+        sftp.file.open('/home/webiva/shared/config/server.yml','w') do |f|
+          f.puts(YAML.dump({ 'server' => { 'name' => server.private_hostname,
+                           'type' => 'slave',
+                           'roles' => server.roles } }))
+
+        end
+
+        ssh.close
+
+      end
+    else
+      servers = deployment.servers
+
+      fail_step("No servers") unless servers.length > 0
+      servers.each do |server|
+        server.ssh do |ssh|
+          ssh.exec!("cd /home/webiva/current; sudo -u webiva ./script/update_server_info.rb")
+        end
+      end
 
     end
 
