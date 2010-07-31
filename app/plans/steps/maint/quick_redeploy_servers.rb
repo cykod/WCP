1298e256b180723783d0d991aa6c5f92d798c0af
Pascal Rettig <pascal@cykod.com>
2010-07-31 16:14:51 -0400

    Quick redeploy

---
+++ app/plans/steps/maint/quick_redeploy_servers.rb
@@ -0,0 +1,23 @@
+
+class Steps::Maint::QuickRedeployServers < Steps::Base
+
+  step_info "(R1-Quick) Rerun the deploy task (No migration)" , :substeps => 2 
+
+  class Options < HashModel
+  end
+
+  def execute!(step)
+
+    if step.substep == 0
+
+    else
+      machines = deployment.servers
+      deployment.cloud.force_redeploy
+
+      client.run_chef_client(machines) 
+
+      deployment.cloud.unforce_redeploy
+    end
+  end
+
+end
