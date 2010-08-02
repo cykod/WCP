3502f523e324594adf4542fc519df79a6be505ba
Pascal Rettig <pascal@cykod.com>
2010-08-02 17:39:24 -0400

    Added restart for just worklings

---
+++ app/plans/steps/maint/restart_workling_servers.rb
@@ -0,0 +1,26 @@
+
+class Steps::Maint::RestartAllWorklingServers < Steps::Base
+
+  step_info "(M2) Restart All Workling Servers in cloud"
+
+  class Options < HashModel
+  end
+
+  def execute!(step)
+    machines = cloud.servers.select(&:active?).select(&:workling_server?)
+
+    session = Net::SSH::Multi.start 
+
+    machines.each do |server|
+      server.multi_ssh(session)
+    end
+
+    cmd = 'cd /home/webiva/current '
+    cmd += " && sudo -u webiva ./script/workling_client stop"
+    cmd += " && sudo -u webiva ./script/workling_client start"
+    session.exec(cmd)
+    session.loop
+    session.close 
+  end
+
+end
